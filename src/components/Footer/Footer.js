07c31c3fd4c51b9e54ff42bd823445ebc99c99b3
VitaliyGavrilov <vitaliy140798gavrilov@mail.ru>
2023-09-06 16:41:26 +0300

    пиксель перфект страницы мейн в 1280

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
       <div className="footer__content app__content">
         <p className="footer__content_year">@ 2023</p>
         <ul className="footer__links">
-          <li>
+          <li className='footer__link'>
             <a href="https://practicum.yandex.ru" target="_blank" rel="noopener noreferrer" className="footer__link">Яндекс.Практикум</a>
           </li>
           <li className="footer__link">
