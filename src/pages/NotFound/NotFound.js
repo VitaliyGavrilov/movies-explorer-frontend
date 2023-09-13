36cef40f35cf7a5964e60c130042df5d8103c61d
VitaliyGavrilov <vitaliy140798gavrilov@mail.ru>
2023-09-14 02:30:17 +0300

    -ошибка на стр. нотфаунд

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './NotFound.css';
 
 function NotFound() {
+  const navigate = useNavigate();
+
+  function backPage() {
+    navigate(-1);
+  };
   return (
     <div className='app__page app__page_gray'>
       <main className='app__main-block'>
         <section className="not-found">
           <h1 className="not-found__title">404</h1>
           <p className="not-found__text">Страница не найдена</p>
-          <Link to="/" className="not-found__link">Назад</Link>
+          <button className="not-found__link" 
+            type="button"
+            name="notFoundButton"
+            onClick={ backPage }>Назад</button>
         </section>
       </main>
     </div>
